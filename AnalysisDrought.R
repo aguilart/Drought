5300d3801485e8f696f0a6f4c18ef092184e8a61
aguilart <calgit@gmail.com>
2017-05-15 13:46:33 +0200

    Updating

--- AnalysisDrought.R
+++ AnalysisDrought.R
@@ -2,3 +2,11 @@
 #####Analysis of drought experiment. Fungal part. This is also part
 #####of the thesis of Daniel and Christian
 ########################################################################
+
+#loading file
+
+BiomassArea<-read.csv("Biomass_Area.csv",header = TRUE, stringsAsFactors = FALSE)
+
+#changing comma to decimal point
+BiomassArea$radius1_cm<-sub("\\,","\\.",BiomassArea$radius1_cm)
+BiomassArea$radius1_cm<-as.numeric(BiomassArea$radius1_cm)
