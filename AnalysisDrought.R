224a39583002f77739dfc46abe4a7755161fca65
aguilart <calgit@gmail.com>
2017-05-15 13:46:33 +0200

    Updating

--- AnalysisDrought.R
+++ AnalysisDrought.R
@@ -2,3 +2,7 @@
 #####Analysis of drought experiment. Fungal part. This is also part
 #####of the thesis of Daniel and Christian
 ########################################################################
+
+#loading file
+
+BiomassArea<-read.csv("Biomass_Area.csv",header = TRUE, stringsAsFactors = FALSE)
